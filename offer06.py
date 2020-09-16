2395f21880427066fd72d6953fc6b5ba09c7cea0
Capricorn-18 <54014562+Capricorn-18@users.noreply.github.com>
2020-09-16 16:20:18 +0800

    commit offer06.py

---
+++ offer06.py
@@ -0,0 +1,16 @@
+"""输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回)"""
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reversePrint(self, head):
+        resList = []
+        while head:
+            resList.append(head.val)
+            head = head.next
+        return resList[::-1]
